2a4bb61901291aa770e3eaa8cba2bf966815f5a2
Ellie Chen <ellie.cheny@gmail.com>
2018-03-03 15:57:09 -0600

    finally, fixed bugs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,49 @@
 var friendData = require("../data/friends");
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendData);
   });
 
-  app.post("/api/friends", function(req, res) {
-      console.log(req.body)
-      friendData.push(req.body);
-      res.json(friendData);
+  app.post("/api/friends", function (req, res) {
+    var finalScore = [];
+    var minIndex;
+    //console.log(req.body)
+    var me = req.body;
+    friendData.forEach(function (friend) {
+      finalScore.push(compareScore(me, friend));
+    })
+    minIndex = findMinPosition(finalScore);
+    friendData.push(req.body);
+    res.json(friendData[minIndex]);
   });
 
-  app.post("/api/clear", function() {
+  app.post("/api/clear", function () {
     friendData = [];
   });
 };
+
+function compareScore(me, friend) {
+  var totalScore = 0;
+  for (var i = 0; i < 10; i++) {
+    totalScore += Math.abs(parseInt(me.scores[i]) - parseInt(friend.scores[i]))
+    //console.log("for"+ i + ":" + Math.abs(parseInt(friend.scores[i])));
+    // console.log("for"+ i + ":" + Math.abs(parseInt(myScore.scores[i])));
+    // console.log("total score" + totalScore);
+    // console.log(totalScore);
+  }
+  return totalScore;
+}
+function findMinPosition(arr) {
+  if (arr.length === 0) {
+    return -1;
+  }
+  var min = arr[0];
+  var minIndex = 0;
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      minIndex = i;
+      min = arr[i];
+    }
+  }
+  return minIndex;
+}
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(port, function(){
     console.log("App listening on port " + port);
